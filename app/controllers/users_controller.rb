409b75c209185fbc568a9a6e4e45a4ef3dcf15c3
Hyung Kim <hjkmines@gmail.com>
2020-08-10 16:19:46 -0600

    finishes controller actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,33 @@ class UsersController < ApplicationController
     end 
 
     def show 
-
+        @user = User.find(params[:id])
+        render json: @user 
     end 
 
     def create 
-
+        @user = User.create(user_params)
+        render json: @user 
     end
 
     def update 
-
-    end 
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        
+        render json: @user 
+    end     
 
     def destroy 
+        @users = User.all 
+        @user = User.find(params[:id])
+        @user.destroy 
+        render json: @users
+    end 
 
+    private
+
+    def user_params
+        params.require(:user).permit([:name, :username, :email])
     end 
 
 end
